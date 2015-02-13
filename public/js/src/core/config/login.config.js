77eb4076dab8706c19565f47743c14367a879c49
knutole <knutole@noerd.biz>
2015-02-13 13:30:23 +0100

    login config

--- public/js/src/core/config/login.config.js
+++ public/js/src/core/config/login.config.js
@@ -1,5 +1,23 @@
 var loginConfig = {
-
-	logo : 'images/griffon_logo_drop.png',
-	
+	// gl : true,
+	autoStart : true,
+	accessToken : 'pk.eyJ1Ijoic3lzdGVtYXBpYyIsImEiOiJkV2JONUNVIn0.TJrzQrsehgz_NAfuF8Sr1Q',
+	layer : 'systemapic.kcjonn12',
+	logo : 'css/images/griffon_logo_drop.png',
+	wrapper : false,
+	speed : 1000,
+	position : {
+		lat : -33.83214, // sydney
+		lng : 151.22299,
+		zoom : [4, 15]
+	},
+	circle : {
+		radius : 120, 
+		color : 'rgba(247, 175, 38, 0.3)',
+		border : {
+			px : 4,
+			solid : 'solid',
+			color : 'white'
+		}
+	}
 }
