ea9b951dfc4a97768b6951a0ce76d0adf0ff4c6b
jorgenevil <jorgenekvoll@gmail.com>
2015-02-16 14:07:26 +0100

    #nohtml merged

---
+++ public/js/src/core/config/login.config.js
@@ -0,0 +1,23 @@
+var loginConfig = {
+	// gl : true,
+	autoStart : true,
+	accessToken : 'pk.eyJ1Ijoic3lzdGVtYXBpYyIsImEiOiJkV2JONUNVIn0.TJrzQrsehgz_NAfuF8Sr1Q',
+	layer : 'systemapic.kcjonn12',
+	logo : 'css/images/systemapic-login-logo.png',
+	wrapper : false,
+	speed : 1000,
+	position : {
+		lat : 59.94338, // oslo
+		lng : 10.71445,
+		zoom : [4, 15]
+	},
+	circle : {
+		radius : 120, 
+		color : 'rgba(247, 175, 38, 0.3)',
+		border : {
+			px : 4,
+			solid : 'solid',
+			color : 'white'
+		}
+	}
+}
