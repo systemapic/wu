e2c37b416f079edfe5c6a49212b96a4046c93a34
jorgenevil <jorgenekvoll@gmail.com>
2015-03-01 14:17:42 +0100

    merge

---
+++ public/js/src/config/login.config.js
@@ -0,0 +1,29 @@
+var loginConfig = {
+	// gl : true,
+	autoStart : true,
+	accessToken : 'pk.eyJ1Ijoic3lzdGVtYXBpYyIsImEiOiJkV2JONUNVIn0.TJrzQrsehgz_NAfuF8Sr1Q',
+	layer : 'systemapic.kcjonn12',
+	logo : 'css/images/systemapic-login-logo.png',
+	wrapper : false,
+	speed : 1000,
+	position : {
+		// lat : -33.83214, // sydney
+		// lng : 151.22299,
+		lat : 59.942,
+		lng : 10.716,
+		zoom : [4, 14]
+	},
+	circle : {
+		radius : 120, 
+		color : 'rgba(247, 175, 38, 0.3)',
+		border : {
+			px : 4,
+			solid : 'solid',
+			color : 'white'
+		}
+	},
+
+	ga : {
+		id : 'UA-57572003-2'
+	}
+}
