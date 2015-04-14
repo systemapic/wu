c8dd838f7338161cd8c1193d5134ebba63ca1b54
knutole <knutolesjoli@gmail.com>
2015-04-14 15:58:19 +0200

    server-config.js, docker

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 // API: api.js
 
 // config
-var config = require('../config/config.js');
+var config = require('../config/server-config.js');
 
 // redis store for temp passwords
 var redisStore = require('redis').createClient(config.temptokenRedis.port, config.temptokenRedis.host);
