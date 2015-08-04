78698eecc83293ec89a361f3242bfadfb981e303
knutole <knutole@noerd.biz>
2015-08-04 20:18:04 +0200

    chrome buttons: carto editor. toggle right chrome

--- public/js/src/chrome/chrome.right.js
+++ public/js/src/chrome/chrome.right.js
@@ -2,7 +2,11 @@ Wu.Chrome.Right = Wu.Chrome.extend({
 
 	_ : 'rightchrome', 
 
-	_initialize : function (options) {
+	options : {
+		defaultWidth : 400,
+	},
+
+	_initialize : function () {
 		console.log('rightchrome init', this);
 
 		// init container
@@ -14,12 +18,59 @@ Wu.Chrome.Right = Wu.Chrome.extend({
 
 	initContainer : function () {
 
+		// create the container (just a div to hold errythign)
 		this._container = Wu.DomUtil.create('div', 'chrome chrome-container chrome-right', app._appPane);
+
+		// create a container to hold the editor
+		this._cartoEditorContainer = Wu.DomUtil.create('div', 'chrome-content cartoeditor', this._container);
+
+		// create other containers for other type content
+		// ...
 
 	},
 
 	addHooks : function () {
+		// todo
+	},
 
+
+	open : function () {
+		console.log('open right chrome');
+		
+		// set width of right pane
+		this._container.style.width = this.options.defaultWidth + 'px';
+
+		// move map
+		this.moveMap('open');
+	},
+
+	close : function () {
+		console.log('close right chrome');
+
+		// set width of right pane
+		this._container.style.width = '0';
+
+		// move map
+		this.moveMap('close');
+	},
+
+	// helper fn, todo: refactor
+	moveMap : function (direction) {
+
+		// div = map container
+		var div = app.MapPane._container;
+
+		// get current map width
+		var currentWidth = div.offsetWidth;
+
+		// default: full width
+		var width = '100%';
+
+		// if open, set width to current minus width of right chrome
+		if (direction == 'open') width = currentWidth - this.options.defaultWidth + 'px';
+
+		// set width
+		div.style.width = width;
 	},
 
 
