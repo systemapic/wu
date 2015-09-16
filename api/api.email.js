7ff38d67199f4fbb538e254b5b544e7a7ea42a72
knutole <knutolesjoli@gmail.com>
2015-09-16 22:35:18 +0200

    regitration email

--- api/api.email.js
+++ api/api.email.js
@@ -337,7 +337,7 @@ module.exports = api.email = {
 		var subtitle = 'Hi ' + name + '. Your account is now active. You currently have read access to projects that have been shared with you. '
 		subtitle += '<br><br>Contact us if you would like to join our beta and create your own projects. You can sign up for our beta on our <a href="http://systemapic.com" target="_blank">website</a>.';
 		subtitle += '<br><br>We hope you will enjoy your new way of sharing and collaborating on geodata. We wish you a great workday on <a href="' + login_link + '">Systemapic.com</a>! <br><br> —Jørgen Ekvoll (CEO)'
-		var link = 'http://systemapic.com/blog';
+		var link = 'http://systemapic.com';
 		console.log('link', link);
 		var link_text = 'Join our beta!';
 
