eb8998e924956020e77fb03d8d0531ebb58f8753
knutole <knutole@noerd.biz>
2015-12-04 23:54:56 +0100

    migrated

--- scripts/migrate_projects.js
+++ scripts/migrate_projects.js
@@ -30,27 +30,146 @@ Project
 .populate('roles')
 .exec(function (err, projects) {
 
-	projects.forEach(function (project) {
+	console.log('projects', projects.length);
 
-		console.log('project: ', project);
+	projects.forEach(function (project, n) {
 
+		// console.log('project: ', n);
 
 		var roles = project.roles;
 
 		// console.log('project with roles:', roles);
 
+		// console.log('PROJECT ACCESS: ', project.access.read);
+
 
 		roles.forEach(function (role) {
 
-			console.log('role ', role);
+			// console.log('role ', role);
 
-			console.log('role.slug', role.slug);
+			// console.log('role.slug', role.slug);
+
+
+			// if (role.slug == 'projectOwner') {
+			// 	console.log('projectOwners: ', role.members);
+
+			// }
+
+			// if (role.slug == 'projectEditor') {
+			// 	console.log('projectEditor: ', role.members);
+			// }
+
+			if (role.slug == 'projectReader') {
+				console.log('projectReader: ', role.members);
+
+				// add read
+				// add_read_to_project(project, role.members, function (err) {
+
+
+					// done!
+
+
+				// });
+
+				if (!role.members) return;
+
+				add_to_contact_list(role.members, function (err) {
+
+					console.log('done!');
+				})
+			}
+
 		});
 
-			process.exit(0);
+
+	});
+
+	// process.exit(0);
+
+});
+
+
+function add_to_contact_list (members, done) {
+
+	var list = [];
+
+	User
+	.findOne({uuid : 'user-a65d8fc1-7016-4638-8468-6e7c0c455ab7'})
+	.exec(function (err, main_user) {
+
+		console.log('main_user contact list: BEFORE ', main_user.contact_list);
+
+
+		async.each(members, function (m, callback) {
+			console.log('m: ', m);
+
+
+			User
+			.findOne({uuid : m})
+			.exec(function (err, user) {
+				if (err || !user) return callback();
+				
+				console.log('user contact list: b4 ', user.contact_list);
+
+				user.contact_list.push(main_user._id);
+				user.save(function (err) {
+					
+					console.log('user contact list: after ', user.contact_list);
+
+					list.push(user._id);
+
+					callback(err);
+
+				});
+			})
+
+
+
+		}, function (err) {
+
+			list.forEach(function (l) {
+				main_user.contact_list.addToSet(l);
+			});
+
+			main_user.save(function (err) {
+
+				console.log('all done!');
+
+				console.log('main_user contact list: AFTER', main_user.contact_list);
+
+				done();
+			});
+
+
+		});
 
 	});
 
 
+}
 
-})
+
+function add_read_to_project (project, members, callback) {
+
+	members.forEach(function (m) {
+		project.access.read.addToSet(m);
+	});
+
+	project.save(function (err) {
+		console.log('project saved!');
+
+		callback(err);
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
