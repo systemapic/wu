2338d16207285fbcb10a956ea6681dabbfbfe3d6
knutole <knutole@systemapic.com>
2015-12-31 00:53:04 +0100

    uplaod script

--- scripts/API/upload_data.sh
+++ scripts/API/upload_data.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 
-USERNAME=your_email
-PASSWORD=your_password
-SERVER=maps.globesar.com
-FILEPATH=/home/tsx_huge.tar.gz
+# USERNAME=knutole@systemapic.com
+# PASSWORD=***REMOVED***
+# SERVER=dev.systemapic.com
+# FILEPATH=/home/r-1e54eed9-0c0d-480c-88a5-decaa4cd35b8-Sag-2010-2340-53670.ecw
+# FILEPATH=$1
+
+
+USERNAME=jorgen@systemapic.com
+PASSWORD=***REMOVED***
+SERVER=dev.systemapic.com
+FILEPATH=/home/r-1e54eed9-0c0d-480c-88a5-decaa4cd35b8-Sag-2010-2340-53670.ecw
 
 # get access token
 ACCESS_TOKEN=$(curl -s --header "Authorization: Basic YWJjMTIzOnNzaC1zZWNyZXQ=" --data "grant_type=password&username=$USERNAME&password=$PASSWORD&scope=offline_access" https://$SERVER/oauth/token | jq -r ".access_token")
