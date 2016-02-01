ccd61f00ef385ad32dacf83178ea668e3072b23d
knutole <knutole@systemapic.com>
2016-02-01 23:24:50 +0100

    enter_db script

--- scripts/postgis/enter_db.sh
+++ scripts/postgis/enter_db.sh
@@ -2,9 +2,6 @@
 
 # get config
 source /systemapic/config/env.sh
-# echo $SYSTEMAPIC_PGSQL_USERNAME
-# echo $SYSTEMAPIC_PGSQL_PASSWORD
-# echo $SYSTEMAPIC_PGSQL_DBNAME
 
 export PAGER="/usr/bin/less -S"
 export PGPASSWORD=$SYSTEMAPIC_PGSQL_PASSWORD 
