da0fdcf7b92aa27b4a52752e23a3e8d8b9ac00f6
knutole <knutolesjoli@gmail.com>
2016-02-05 17:13:56 +0100

    sourcing config in start-server.sh; renamed server.sh->start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,33 +1,23 @@
 #!/bin/bash
 
-# if [ "$1" == "prod" ];then
-# 	SYSTEMAPIC_PRODMODE=true
-# else 
-# 	SYSTEMAPIC_PRODMODE=false	
-# fi;
+# source config
+source /systemapic/config/env.sh
 
-
-# env set in docker-compose.yml
-echo 'Prod mode?'
-echo $SYSTEMAPIC_PRODMODE 
-
-cd server
-
-
+# start prodmode
 if $SYSTEMAPIC_PRODMODE; then
-
+	cd server
 	echo 'Production mode'
 	grunt prod 
 	echo 'Running in production mode...'
 	forever server.js prod
 
+# start dev mode
 else
+	cd server
 	echo 'Development mode'
 	grunt dev 
 	grunt watch &
 	echo 'Running in development mode...'
 	nodemon --watch ../api --watch /systemapic/config --watch server.js --watch ../routes server.js
-	
 fi
-
 cd ..
