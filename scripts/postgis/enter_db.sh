aafd4f40bd3a9d6d7d5011c29544c84344dc607d
Sandro Santilli <strk@keybit.net>
2016-02-18 12:18:13 +0100

    Allow specifying psql parameters
    For example to connect to a specific database, or to run queries

--- scripts/postgis/enter_db.sh
+++ scripts/postgis/enter_db.sh
@@ -7,4 +7,7 @@ source /systemapic/config/env.sh
 
 export PAGER="/usr/bin/less -S"
 export PGPASSWORD=$SYSTEMAPIC_PGSQL_PASSWORD 
-psql -h postgis --username=$SYSTEMAPIC_PGSQL_USERNAME template1
+export PGUSER=$SYSTEMAPIC_PGSQL_USERNAME
+export PGDATABASE=template1
+
+psql -h postgis $@
