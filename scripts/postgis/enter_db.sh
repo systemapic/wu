ed65d89decd693662ac332e40d355b64624450e3
Sandro Santilli <strk@keybit.net>
2016-02-18 11:54:22 +0100

    Allow calling script from outside the container

--- scripts/postgis/enter_db.sh
+++ scripts/postgis/enter_db.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# We assume there's no "docker" command inside the docker
+which docker && {
+  echo "We're probably outside the wu docker"
+  test -n "$SYSTEMAPIC_DOMAIN" || {
+    echo "Please export SYSTEMAPIC_DOMAIN env variable" >&2
+    exit 1
+  }
+  container=${SYSTEMAPIC_DOMAIN}_wu_1
+  echo "Executing myself inside container ${container}"
+  exec docker exec -ti ${container} \
+    ./scripts/postgis/enter_db.sh
+}
+
 # get config
 source /systemapic/config/env.sh
 
