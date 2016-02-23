0414dfb9141b4c48469abbfc6f368ffbd636f91b
knutole <knutolesjoli@gmail.com>
2016-02-23 09:05:01 +0100

    phantomJS - #322

--- tools/phantomJS-snapshot.js
+++ tools/phantomJS-snapshot.js
@@ -1,191 +1,272 @@
-// create page
-var page = require('webpage').create(),
-	system = require('system'),
-	address, output, size;
-
+var WebPage = require('webpage');
+var system = require('system');
+var url = 'https://dev.systemapic.com';
 // parse args
+
+console.log('systyem', system.args)
+
 var args = JSON.parse(system.args[1]);
 
-// get vars
-var projectUuid = args.projectUuid;
-var hash        = args.hash;
-var path        = args.path;
-var isPdf 	= args.pdf;
-var isThumb     = args.thumb;
-var serverUrl   = args.serverUrl;
-var serverData  = args.serverData;
-var outfile 	= path;
+console.log('args: ', args);
 
-// console.log('a2rgs: ', args);
+var outPath = args.outPath || '/tmp/screenshot.png';
+console.log('outPath', outPath);
 
-// console.log('SERVER URL :', serverUrl);
-// console.log('DATA: ', serverData);
-
-// console.log('isPdf', isPdf);
-// console.log('outfile', outfile);
-
-// connect
-var page = require('webpage').create(),
-	server = serverUrl,
-	data = serverData;    // phantomJS account
+var url = args.url;// || 'https://dev.systemapic.com/';
+var view_string = JSON.stringify(args.view);// || {"project_id":"project-67c0fa3b-723d-43d6-9a09-56ce12cec6c7","position":{"lat":0,"lng":73.564453125,"zoom":3},"layers":["layer-b554dc0d-a61d-4684-aed8-fb22c016e89e","layer-5b6b209a-76c0-4313-af6b-762ffc9681e0","layer-061984fb-367c-4ee9-ac6c-84696276fe6b"]};
 
 
-page.onCallback = function(data){
-	console.log("finished: " + data.text);
-	page.viewportSize = { width : 1620, height: 1080 };
-	page.render(outfile);
-	phantom.exit();
-    // phantom.exit();
-};
+console.log('url:', url);
+console.log('view: ', view_string);
 
-									// todo: create phantomjs user with changing password..
-// revv phantomjs
+page = WebPage.create();
 page.viewportSize = { width : 1620, height: 1080 };	// set size...
-page.open(server, 'get', function (status, why) {
+page.open(url, 'get', function (status, why) {
 	
-	// console.log('phtantomJS open!', status)
-	
-	// if not success, exit
-	if (status !== 'success') return phantom.exit();
-	
-	// console.log('PHTANTOM SUCCESS!!');
+	console.log('status: ', status);
+
+	// pings from client
+	page.onCallback = function(data){
+
+		// feedback
+		console.log("ping: " + data.text);
+
+		// all done, capture!
+		if (data.text == 'ready') {
+			console.log('Capturing...');
+			page.render(outPath);
+			phantom.exit();
+		}
+	};
+
+	// check for app, and develop view when ready
+	setInterval(function () {
+		page.evaluate(function (view_string) {
+			if (!window.app || !window.app.phantomjs) return;
+			if (window.app._phantomInited) return;
+			window.app._phantomInited = true;
+			var view = Wu.parse(view_string);
+
+			window.app.tokens.access_token = view.access_token;
+			window.app.phantomjs.develop(view);
+		});
+	}, 500);
+
+});
 
 
-	// // // interact with page
-	// page.evaluate(function(args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // create page
+// var page = require('webpage').create(),
+// 	system = require('system'),
+// 	address, output, size;
+
+// // parse args
+// var args = JSON.parse(system.args[1]);
+
+// // get vars
+// var projectUuid = args.projectUuid;
+// var hash        = args.hash;
+// var path        = args.path;
+// var isPdf 	= args.pdf;
+// var isThumb     = args.thumb;
+// var serverUrl   = args.serverUrl;
+// var serverData  = args.serverData;
+// var outfile 	= path;
+
+// // console.log('a2rgs: ', args);
+
+// // console.log('SERVER URL :', serverUrl);
+// // console.log('DATA: ', serverData);
+
+// // console.log('isPdf', isPdf);
+// // console.log('outfile', outfile);
+
+// // connect
+// var page = require('webpage').create(),
+// 	server = serverUrl,
+// 	data = serverData;    // phantomJS account
+
+
+// page.onCallback = function(data){
+// 	console.log("finished: " + data.text);
+// 	page.viewportSize = { width : 1620, height: 1080 };
+// 	page.render(outfile);
+// 	phantom.exit();
+//     // phantom.exit();
+// };
+
+// 									// todo: create phantomjs user with changing password..
+// // revv phantomjs
+// page.viewportSize = { width : 1620, height: 1080 };	// set size...
+// page.open(server, 'get', function (status, why) {
+	
+// 	// console.log('phtantomJS open!', status)
+	
+// 	// if not success, exit
+// 	if (status !== 'success') return phantom.exit();
+	
+// 	// console.log('PHTANTOM SUCCESS!!');
+
+
+// 	// // // interact with page
+// 	// page.evaluate(function(args) {
 		
-	// 	// init for phantomJS
-	// 	if (app) app._setPhantomArgs(args);
+// 	// 	// init for phantomJS
+// 	// 	if (app) app._setPhantomArgs(args);
 
-	// }, args);
+// 	// }, args);
 
 
 
-	// console.log('phantom after evaluate...');
+// 	// console.log('phantom after evaluate...');
 
-	waitFor(function () {
-		// console.log('checking... STRINGIFIED =>', JSON.stringify(args));
+// 	waitFor(function () {
+// 		// console.log('checking... STRINGIFIED =>', JSON.stringify(args));
 
-		console.log('    SUNGLASSES ---->>>     ')
-		console.log('args.access_token', args.access_token);
-		console.log('args.projectUuid', args.projectUuid);
+// 		console.log('    SUNGLASSES ---->>>     ')
+// 		console.log('args.access_token', args.access_token);
+// 		console.log('args.projectUuid', args.projectUuid);
 
-		// Check in the page if a specific element is now visible
-		var result = page.evaluate(function(args) {
+// 		// Check in the page if a specific element is now visible
+// 		var result = page.evaluate(function(args) {
 
-			console.log('evaluating!');
+// 			console.log('evaluating!');
 
-			if (!app) return 'no app';
-			// if (!app.Projects) return 'no projects';
+// 			if (!app) return 'no app';
+// 			// if (!app.Projects) return 'no projects';
 
 			
 
 
-			var p = new Wu.PhantomJS();
+// 			var p = new Wu.PhantomJS();
 
-			p.get({
-				project_id : args.projectUuid,
-				user_access_token : args.access_token
-			});
+// 			p.get({
+// 				project_id : args.projectUuid,
+// 				user_access_token : args.access_token
+// 			});
 
-			// if (!app._isPhantom) {
-			// 	setTimeout(function () {
+// 			// if (!app._isPhantom) {
+// 			// 	setTimeout(function () {
 
-			// 		app.phantomJS(args);
-			// 	}, 1000);
-			// }
-			// return app.phantomReady();
-			return false; // debug, will timeout
-		}, args);
+// 			// 		app.phantomJS(args);
+// 			// 	}, 1000);
+// 			// }
+// 			// return app.phantomReady();
+// 			return false; // debug, will timeout
+// 		}, args);
 
-		console.log('result: ', result);
+// 		console.log('result: ', result);
 
-		return result === true;
-	}, 
+// 		return result === true;
+// 	}, 
 
-	// callback
-	function () {
+// 	// callback
+// 	function () {
 		
-		console.log('loaded!!1');
+// 		console.log('loaded!!1');
 
-		setTimeout(function () {
-			page.viewportSize = { width : 1620, height: 1080 };
-			page.render(outfile);
-			phantom.exit();
-		}, 5000);
+// 		setTimeout(function () {
+// 			page.viewportSize = { width : 1620, height: 1080 };
+// 			page.render(outfile);
+// 			phantom.exit();
+// 		}, 5000);
 
 		
 
-	});
+// 	});
 	
-	// waitFor(function () {
-	// 	console.log('checking...', JSON.stringify(args));
+// 	// waitFor(function () {
+// 	// 	console.log('checking...', JSON.stringify(args));
 
-	// 	// Check in the page if a specific element is now visible
-	// 	var result = page.evaluate(function(args) {
+// 	// 	// Check in the page if a specific element is now visible
+// 	// 	var result = page.evaluate(function(args) {
 
-	// 		console.log('evaluating!');
+// 	// 		console.log('evaluating!');
 
-	// 		if (!app) return 'no app';
-	// 		if (!app.Projects) return 'no projects';
+// 	// 		if (!app) return 'no app';
+// 	// 		if (!app.Projects) return 'no projects';
 
-	// 		if (!app._isPhantom) {
-	// 			setTimeout(function () {
+// 	// 		if (!app._isPhantom) {
+// 	// 			setTimeout(function () {
 
-	// 				app.phantomJS(args);
-	// 			}, 1000);
-	// 		}
-	// 		return app.phantomReady();
-	// 	}, args);
+// 	// 				app.phantomJS(args);
+// 	// 			}, 1000);
+// 	// 		}
+// 	// 		return app.phantomReady();
+// 	// 	}, args);
 
-	// 	console.log('result: ', result);
+// 	// 	console.log('result: ', result);
 
-	// 	return result === true;
-	// }, 
+// 	// 	return result === true;
+// 	// }, 
 
-	// // callback
-	// function () {
+// 	// // callback
+// 	// function () {
 		
-	// 	console.log('loaded!!1');
+// 	// 	console.log('loaded!!1');
 
-	// 	setTimeout(function () {
-	// 		page.viewportSize = { width : 1620, height: 1080 };
-	// 		page.render(outfile);
-	// 		phantom.exit();
-	// 	}, 5000);
+// 	// 	setTimeout(function () {
+// 	// 		page.viewportSize = { width : 1620, height: 1080 };
+// 	// 		page.render(outfile);
+// 	// 		phantom.exit();
+// 	// 	}, 5000);
 
 		
 
-	// });
-});
+// 	// });
+// });
 
 
-// waitFor helper fn
-function waitFor(testFx, onReady, timeOutMillis) {
-	var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 10000, //< Default Max Timout is 3s
-		start = new Date().getTime(),
-		condition = false,
-		interval = setInterval(function() {
-			if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
-				// If not time-out yet and condition not yet fulfilled
-				condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
-			} else {
-				if(!condition) {
-					// If condition still not fulfilled (timeout but condition is 'false')
-					console.log("'waitFor()' timeout");
-					page.viewportSize = { width : 1620, height: 1080 };
-					page.render(outfile);
-					phantom.exit();
-					// phantom.exit(1);
-				} else {
-					// Condition fulfilled (timeout and/or condition is 'true')
-					console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-					typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
-					clearInterval(interval); //< Stop this interval
-				}
-			}
-		}, 250); //< repeat check every 250ms
-};
+// // waitFor helper fn
+// function waitFor(testFx, onReady, timeOutMillis) {
+// 	var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 10000, //< Default Max Timout is 3s
+// 		start = new Date().getTime(),
+// 		condition = false,
+// 		interval = setInterval(function() {
+// 			if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+// 				// If not time-out yet and condition not yet fulfilled
+// 				condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+// 			} else {
+// 				if(!condition) {
+// 					// If condition still not fulfilled (timeout but condition is 'false')
+// 					console.log("'waitFor()' timeout");
+// 					page.viewportSize = { width : 1620, height: 1080 };
+// 					page.render(outfile);
+// 					phantom.exit();
+// 					// phantom.exit(1);
+// 				} else {
+// 					// Condition fulfilled (timeout and/or condition is 'true')
+// 					console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+// 					typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
+// 					clearInterval(interval); //< Stop this interval
+// 				}
+// 			}
+// 		}, 250); //< repeat check every 250ms
+// };
 
 
 
