f10da60360acc4ed6509d59f4957c0f1f1527c0c
IgorZiegler <icygler@hwdtech.ru>
2016-03-10 21:42:32 +0600

    Fixed apidoc name, move create_docs.js script to script directory and rename create_docs.sh to gen_docs.sh

--- scripts/create_docs.js
+++ scripts/create_docs.js
@@ -1,9 +1,9 @@
 var fs = require('fs-extra');
 var domain =  'https://' + process.env.SYSTEMAPIC_DOMAIN;
-var config = fs.readJsonSync('apidocs.json');
+var config = fs.readJsonSync('../apidoc.json');
 
 //update the object
 config.sampleUrl = domain;
 
 //update the file with new object
-fs.outputJsonSync('apidocs.json', config);
+fs.outputJsonSync('../apidoc.json', config);
