43cc9b6cdb0e8799da656d60938bfa54a72d6eed
jorgen <jorgen@systemapic.com>
2016-04-12 19:38:16 +0200

    separating slider and graph

--- Gruntfile.js
+++ Gruntfile.js
@@ -230,6 +230,7 @@ module.exports = function(grunt) {
 					'public/js/src/panes/pane.account.js',
 					'public/js/src/panes/pane.guide.js',
 					'public/js/src/panes/pane.bigslider.js',
+					'public/js/src/panes/pane.bigsliderGraph.js',
 
 					// chrome
 					'public/js/src/chrome/chrome.js',	
